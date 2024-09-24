e4458efe22cf6d95e1e9136c5f4caa07aab28e82
Kelly <158290487+kya512@users.noreply.github.com>
2024-09-24 10:24:36 +0200

    mise a jour ecole

--- 19-POO/19_1-Declaration_classe/P19_1_c-Surface/src/main/kotlin/main.kt
+++ 19-POO/19_1-Declaration_classe/P19_1_c-Surface/src/main/kotlin/main.kt
@@ -3,13 +3,13 @@ class Rectangle {
     var height: Int = 0
 }
 
-fun printArea(rectangle: Rectangle) {
-    return 
+fun printArea(rectangle: Rectangle): Int {
+    return(rectangle.width*rectangle.height)
 }
 
 fun main() {
-    val rectlangle = Rectangle()
-    rectlangle.width = 5
-    rectlangle.height = 3
-    printArea(rectlangle)
+    val rectangle = Rectangle()
+    rectangle.width = 5
+    rectangle.height = 3
+    println(printArea(rectangle))
 }
